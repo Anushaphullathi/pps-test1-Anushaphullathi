27d351cac4e561b82caeec95f5b281cb4aeb6587
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-01-19 17:07:39 +0000

    p0 anusha

--- p3original.c
+++ p3original.c
@@ -1,4 +1,4 @@
-gd#include<stdio.h>
+#include<stdio.h>
 void input(int *a,int *b)
 {
 printf("enter the value of two numbers\n");
---
+++ p7original.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void input_array(int n,int a[n]);
+{
+  int i;
+  printf("enter the size of array\n");
+  scanf("%d",i);
+
+}
+int sum_n_arrays(int n,int a[n]);
+{
+
+}
+void out_put(int n,int a[n],int sum);
+{
+
+}
+int main()
+{
+  return 0;
+}
